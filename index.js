93ff7ca4419ffda18494637df6e98d2232e0cca6
john million <jpmillion@comcast.net>
2021-06-26 12:55:58 -0400

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
 function findMinAndRemove(array){
-
+    let min = array[0];
+    let minIdx = 0;
+    for (let i = 1; i < array.length; i++) {
+        if (array[i] < min) {
+            min = array[i];
+            minIdx = i;
+        }
+    }
+    return array.splice(minIdx, 1);
 }
 
 function selectionSort(array){
-
+    const sorted = [];
+    while (array.length) {
+        let min = findMinAndRemove(array);
+        sorted.push(...min);
+    }
+    return sorted;
 }
